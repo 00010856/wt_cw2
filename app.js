1b353885b9c7d283e0d3d4a6105ea04d695ee64c
00010856 <00010856wiut@gmail.com>
2023-03-27 20:23:20 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const {config, engine} = require('express-edge');
+app.use(express.static('public'));
+app.use(engine);
+app.set('views', `${__dirname}/views`);
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+app.listen(5000, () => {
+    console.log('Server started on port 5000');
+});
